9d28874a13b2d267554946bccd5cf74f4cf7bfc3
V1G4 <lego.architecte@gmail.com>
2023-05-22 16:50:03 +0700

    housekeeping

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import markdoc from "@astrojs/markdoc";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), markdoc()]
+  integrations: [ mdx(), markdoc() ]
 });
