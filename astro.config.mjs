8274d98ce6f82d72d7e71028162c338188703f0a
V1G4 <lego.architecte@gmail.com>
2023-07-24 00:07:27 +0700

    mdx-migration
    Changing back from markdoc to mdx because syntax and integration is more seamless

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-import markdoc from "@astrojs/markdoc";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [ mdx(), markdoc() ]
+  integrations: [mdx()]
 });
