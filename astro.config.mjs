de2bd91ff2e5cbf0cff41e21236382f612dedfc2
Humanbeingdood <113927211+Humanbeingdood@users.noreply.github.com>
2023-08-10 22:06:24 +0700

    hope it worky

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,7 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()]
+  site: 'https://ACSdemy.github.io',
+//  base: '/ACSdemy',
+//  integrations: [mdx()]
 });
